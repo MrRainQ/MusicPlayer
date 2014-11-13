38e7898985039f88aa0237ff1f3b772fac2eb830
sen5labs <sen5labs@sen5.local>
2014-11-13 18:49:15 +0800

    Initial Commit

---
+++ RQMusic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RQMusic
+//
+//  Created by sen5labs on 14-11-13.
+//  Copyright (c) 2014年 sen5labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RQMusic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RQMusic
+//
+//  Created by sen5labs on 14-11-13.
+//  Copyright (c) 2014年 sen5labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
